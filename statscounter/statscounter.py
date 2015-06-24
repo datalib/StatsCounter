102c0848dbba6691d697fca6c0c4148fe6fc28b1
im-rodrigo <rodrigopala91@gmail.com>
2015-06-24 16:52:51 -0400

    changed 'pdist' to 'normalize' as it's the correct name of the operation

--- statscounter/statscounter.py
+++ statscounter/statscounter.py
@@ -91,7 +91,15 @@ class StatsCounter(Counter):
 		_, value = self.best_pair()
 		return value
 
-	def pdist(self):
+	def normalize(self):
+		"""
+		Sum the values in a Counter, then create a new Counter 
+		where each new value (while keeping the original key) 
+		is equal to the original value divided by sum of all the
+		original values (this is sometimes referred to as the 
+		normalization constant). 
+		https://en.wikipedia.org/wiki/Normalization_(statistics)
+		"""
 		total = sum(self.values())
 		stats = {k: (v / float(total)) for k, v in self.items()}
 		return StatsCounter(stats)
--- tests/test_statscounter.py
+++ tests/test_statscounter.py
@@ -51,8 +51,8 @@ class TestStatsCounter:
 		m = self.counter_ints.max()
 		assert m == 999
 
-	def test_pdist(self):
-		pdist = StatsCounter({1: 1, 2: 2, 3: 1}).pdist()
+	def test_normalize(self):
+		pdist = StatsCounter({1: 1, 2: 2, 3: 1}).normalize()
 		assert pdist == {
 			1: 0.25,
 			2: 0.50,
