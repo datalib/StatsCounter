7ad446d14de6712b9a55b9282272887d256bf215
im-rodrigo <rodrigopala91@gmail.com>
2015-07-21 23:28:46 -0400

    upped vs. number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def readme():
 
 setup(
     name='statscounter',
-    version='0.0.010',
+    version='0.0.011',
     url='https://github.com/datalib/statscounter',
     license='MIT',
     description="Python's missing statistical Swiss Army knife",
